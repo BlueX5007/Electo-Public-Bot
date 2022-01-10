e11184169913d36b9a77687e2a473eccb569b878
BlueX5007 <92089163+BlueX5007@users.noreply.github.com>
2022-01-10 12:54:09 +0530

    Update fun.py
    Added new commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,5 +8,21 @@ class Fun(commands.Cog):
   @commands.command()
   async def sus(ctx, member: discord.Member):
     x = random.randrange(1, 100)
-    await ctx.send(f'{member} is {x}% sus')
+    await ctx.send(f"{member} is {x}% sus.")
   @commands.command()
+  async def howgay(ctx, member: discord.Member=None):
+    if member == None:
+      member = ctx.author
+    x = random.randrange(1, 100)
+    await ctx.send(f"{member} is {x}% gay.")
+  @commands.command()
+  async def _8ball(ctx, question):
+    responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+             "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+             "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
+             "Yes.", "Yes, definitely.", "You may rely on it."]
+    response = random.choice(responses)
+    embed = discord.Embed(title="My Response", color=discord.Color.dark_magenta())
+    embed.add_field(name="Your question", value=f"{question}")
+    embed.add_field(name="My Response", value=f"{response} _")
+    await ctx.send(embed=embed)
