119a9e57da0dd420a6d616b934a35586f9f6f42e
BlueX5007 <92089163+BlueX5007@users.noreply.github.com>
2022-01-10 12:55:01 +0530

    Update main.py
    Bugs fixed

--- main.py
+++ main.py
@@ -1,38 +1,28 @@
-import discord
 from discord.ext import commands
+import discord
+import random
 import asyncio
-from config import token, prefix, ownerid
-import time
-
-intents=discord.Intents.all()
-bot = commands.Bot(command_prefix=prefix, intents=intents)
-
-@bot.command()
-async def restart(ctx):
-  if ctx.author.id == ownerid:
-    message = await ctx.send('Checking files...')
-    time.sleep(3)
-    await message.edit(content='Unloading packages...')
-    time.sleep(2)
-    await message.edit(content='Restarting...')
-    bot.unload_extension('cogs.fun')
-    time.sleep(10)
-    bot.load_extension('cogs.fun')
-    await message.edit(content='Restarted')
-  else:
-    await ctx.send("You aren't the owner!")
-   
-@bot.command()
-async def shutdown(ctx):
-  if ctx.author.id == ownerid:
-    await ctx.send('Shutting down...')
-    bot.unload_extension('cogs.fun')
-    
-@bot.command()
-async def start(ctx):
-  if ctx.author.id == ownerid:
-    message = await ctx.send('Starting Electro...')
-    bot.load_extension('cogs.fun')
-    await message.edit(content='Started!')
-    
-bot.run(token)
+class Fun(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+  @commands.command()
+  async def sus(ctx, member: discord.Member):
+    x = random.randrange(1, 100)
+    await ctx.send(f"{member} is {x}% sus.")
+  @commands.command()
+  async def howgay(ctx, member: discord.Member=None):
+    if member == None:
+      member = ctx.author
+    x = random.randrange(1, 100)
+    await ctx.send(f"{member} is {x}% gay.")
+  @commands.command()
+  async def _8ball(ctx, question):
+    responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+             "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+             "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
+             "Yes.", "Yes, definitely.", "You may rely on it."]
+    response = random.choice(responses)
+    embed = discord.Embed(title="My Response", color=discord.Color.dark_magenta())
+    embed.add_field(name="Your question", value=f"{question}")
+    embed.add_field(name="My Response", value=f"{response} _")
+    await ctx.send(embed=embed)
